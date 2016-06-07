2b01c53e1ea7c8549b514467eea532563f0d7ea5
Axel Bruneaux <gazhrot@gmail.com>
2016-06-07 16:58:51 +0200

    creation d'un module perso pour gerer l'upload des snaps.

---
+++ modules/upload.js
@@ -0,0 +1,12 @@
+var multer     = require('multer');
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+var upload = multer({ storage : storage}).single('snap');
+
+module.export = upload;
