bb1fe24f136a8625ed0c4d56145716ff81b170a6
Axel Bruneaux <gazhrot@gmail.com>
2016-06-07 17:01:03 +0200

    nouvel version du server.js avec toutes les nouvelles routes et tout les includes des routes/controllers.

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ var qs         = require('querystring');
 var formidable = require("formidable");
 var util       = require('util');
 var bodyParser = require('body-parser');
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'api_snapchat'
-});
+//var connection = require('./modules/mysql');
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -40,141 +34,16 @@ var storage =   multer.diskStorage({
   }
 });
 
-var rand = function() {
-    return Math.random().toString(36).substr(2); // remove `0.`
-};
-
-var token = function() {
-    return rand() + rand(); // to make it longer
-};
-
 var upload = multer({ storage : storage}).single('snap');
 
-app.post('/option=inscription', function(req, res) {
+app.use('/user'    , require('./routes/inscription_route.js'));
+app.use('/user'    , require('./routes/connection_route.js'));
+app.use('/user'    , require('./routes/alluser_route.js'));
+app.use('/snap'    , require('./routes/sendsnap_route.js'));
+app.use('/snap'    , require('./routes/getsnap_route.js'));
+app.use('/snap'    , require('./routes/viewsnap_route.js'));
+app.use('/uploads' , require('./routes/uploadsnap_route.js'));
 
-  if (req.body.email && req.body.password) {
-    var aquery = connection.query('SELECT * FROM user WHERE email = ?', req.body.email, function (err, rows) {
-      if (err) {
-        console.log('fail to find users');
-      } else {
-        console.log(rows.length);
-        if (rows.length < 1) {
-          var aquery = connection.query('INSERT INTO user (email, password) VALUES (?, ?)', [req.body.email, req.body.password], function(err, rows) {
-            if (err) {
-              console.log(err);
-            } else {
-              res.send(JSON.stringify({error: true, data: [], token: ''}));
-            }
-          });
-        } else {
-          res.send(JSON.stringify({error: 'Email ou Password invalide. ou Email deja utiliser.', data: []}));
-        }
-      }
-    });
-  }
-});
-
-app.post('/option=connexion', function(req, res) {
-
-  var tokens = token();
-
-  if (req.body.email && req.body.password) {
-    var aquery = connection.query('SELECT * FROM user WHERE email = ?', req.body.email, function (err, rows) {
-      if (err) {
-        res.send(JSON.stringify({error: 'Email ou Password invalide.', data: []}));
-      } else {
-          var data = {
-            id: rows[0].id,
-            email: rows[0].email
-          }
-          var bquery = connection.query('UPDATE user SET token = ? WHERE email = ?', [tokens, req.body.email], function (err, rows) {
-            if (err) {
-              res.send(JSON.stringify({error: 'Email invalide.', data: []}));
-            } else {
-              res.send(JSON.stringify({error: true, data: JSON.stringify(data), token: tokens}));
-            }
-          });
-      }
-    });
-  }
-});
-
-app.post('/option=alluser', function(req, res) {
-
-  if (req.body.email && req.body.token) {
-    var aquery = connection.query('SELECT * FROM user WHERE email = ? AND token = ?', [req.body.email, req.body.token], function (err, rows) {
-      if (err) {
-        res.send(JSON.stringify({error: 'Email ou Token invalide.', data: []}));
-      } else {
-        if (req.body.token == rows[0].token) {
-          var bquery = connection.query('SELECT * FROM user', function (err, rows) {
-            if (err) {
-              res.send(JSON.stringify({error: 'Aucun utilisateur.', data: []}));
-            } else {
-              
-              var user = [];
-              
-              for (var i = 0; i < rows.length;i++) {
-                user.push({
-                  id: rows[i].id,
-                  email: rows[i].email
-                })
-              }
-              console.log(user);
-              res.send(JSON.stringify({error: true, data: JSON.stringify(user), token: ''}));
-            }
-          });
-        }
-      }
-    });
-  }
-});
-
-app.get('/uploads/:snap', function(req, res) {
-  res.sendFile(__dirname + '/uploads/' + req.params.snap);
-});
-
-app.post('/option=sendsnap', function(req, res, next) {
-
-  upload(req,res,function(err) {
-    if(err) {
-      res.send(JSON.stringify({error: 'Impossible d\'upload la photo', data: []}));
-    }
-    
-    var snapurl = 'http://localhost:3000/uploads/'+req.file.filename;
-    
-    if (req.body.email && req.body.token && req.body.temps && req.body.u2) {
-      var aquery = connection.query('INSERT INTO snapchat (email_sender, id_receiver, url, duration, view) VALUES (?, ?, ?, ?, ?)', [req.body.email, req.body.u2, snapurl, req.body.temps, 0], function(err, rows) {
-        if (err) {
-          res.send(JSON.stringify({error: 'Il y a une erreur quelque part mdr.', data: []}));
-        } else {
-          res.send(JSON.stringify({error: true, data: 'Snap bien envoyer', token: ''}));
-        }
-      });
-    }
-  });
-});
-
-app.post('/option=getsnap', function(req, res) {
-
-  var reponse = {
-    error: 'test',
-    data: req.body.data,
-    token: req.body.token
-  }
-
-  res.send(JSON.stringify(reponse));
-
-});
-
-app.post('/option=viewsnap', function(req, res) {
-
-  /*if (req.body.email && req.body.token && req.body.id) {
-    var aquery = connection.query('');
-  }*/
-
-});
-
-app.listen(3000,function(){
-    console.log("Working on port 3000");
+app.listen(5000,function(){
+    console.log("Working on port 5000");
 });
